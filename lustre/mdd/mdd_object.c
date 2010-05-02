137b631dd82aff3e15220f5371cf223dfc684c10
Alex Zhuravlev <bzzz@sun.com>
2010-05-03 00:44:17 +0400

    missed declaration of xattr

--- lustre/mdd/mdd_object.c
+++ lustre/mdd/mdd_object.c
@@ -1267,7 +1267,9 @@ mdd_declare_and_start_attr_set(const struct lu_env *env, struct md_object *obj,
         }
 
         /* XXX: use appropriate len? */
-        rc = mdo_declare_xattr_set(env, mdd_obj, 4096, XATTR_NAME_ACL_ACCESS, 0, handle);
+        buf.lb_len = 4096;
+        rc = mdo_declare_xattr_set(env, mdd_obj, &buf,
+                                   XATTR_NAME_ACL_ACCESS, 0, handle);
         if (rc)
                 GOTO(out, rc);
 
